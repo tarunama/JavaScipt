cbebe6686261636a7bcdbf18d40e80eb5ffbef0b
meganeski <sakeharami@gmail.com>
2014-10-28 23:49:17 +0900

    javascript_has_5_types

---
+++ basic_knowledge/type.js
@@ -0,0 +1,11 @@
+var string = 'hoge'; // Type String
+var integer = 100; // Type Interger
+var bool = true; // Type Boolean
+var n = null; // Type NULL
+var undefined = ''; // Type Undefined
+
+print(typeof string);
+print(typeof integer);
+print(typeof bool);
+print(typeof n);
+print(typeof undefined);
