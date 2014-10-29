48568e6af325749c26ec1243acd820c7ac06fc1d
meganeski <sakeharami@gmail.com>
2014-10-29 14:19:54 +0900

    type_of_js

--- basic_knowledge/type.js
+++ basic_knowledge/type.js
@@ -3,9 +3,11 @@ var integer = 100; // Type Interger
 var bool = true; // Type Boolean
 var n = null; // Type NULL
 var undefined = ''; // Type Undefined
+function hoge() {}; // Type Function
 
 print(typeof string);
 print(typeof integer);
 print(typeof bool);
 print(typeof n);
 print(typeof undefined);
+print(typeof hoge);
