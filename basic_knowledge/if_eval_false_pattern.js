28b378baec47fd41cdea5543171b9208bcd78adf
meganeski <sakeharami@gmail.com>
2014-10-29 22:41:42 +0900

    false_pattern

---
+++ basic_knowledge/if_eval_false_pattern.js
@@ -0,0 +1,6 @@
+// ALL print false
+if (0) { print('true'); } else { print('false'); }
+if (NaN) { print('true'); } else { print('false'); }
+if (null) { print('true'); } else { print('false'); }
+if (undefined) { print('true'); } else { print('false'); }
+if ('') { print('true'); } else { print('false'); }
